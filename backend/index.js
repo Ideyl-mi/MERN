9e8b65a1b1beebc0e88c433e4815e20f25163e4b
Ideyl-mi <animeyedi@gmail.com>
2026-02-12 19:11:28 +0545

    Kaam bhooo

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 // CORS configuration - MUST be before routes
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-zx7p.vercel.app",
     credentials: true,
   }),
 );
